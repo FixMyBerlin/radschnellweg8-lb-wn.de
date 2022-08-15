3b0bd8c4133e05e302a93bbe9832d5a2c8a33e55
Henri <henri@fixmycity.de>
2022-08-15 15:11:02 +0200

    Use FixMyCity Imprint for now

--- src/components/Baustelle/Baustelle.tsx
+++ src/components/Baustelle/Baustelle.tsx
@@ -84,7 +84,7 @@ export const Baustelle = () => {
           <div className="mx-auto w-full max-w-7xl px-4 py-16 sm:px-6 lg:px-8">
             <nav className="flex space-x-4">
               <a
-                href="/impressum"
+                href="https://fixmycity.de/impressum"
                 className="text-sm font-medium text-gray-500 hover:text-gray-600"
               >
                 Impressum
