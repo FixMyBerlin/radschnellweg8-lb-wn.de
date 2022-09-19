cf1ea569fd11afc3c7e289ba925d7de2b070672b
Tobias <tobias@fixmycity.de>
2022-09-19 10:43:23 +0200

    Add 404 Page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,35 @@
-import { PageProps } from 'gatsby'
+import { Link, PageProps } from 'gatsby'
 import React from 'react'
-import { Baustelle } from '~/components/Baustelle'
+import { HelmetSeo } from '~/components/HelmetSeo'
+import { LayoutArticle } from '~/components/Layout'
 
-const IndexPage: React.FC<PageProps> = () => {
-  return <Baustelle />
+const NotFound: React.FC<PageProps> = ({ location }) => {
+  return (
+    <LayoutArticle location={location}>
+      <HelmetSeo noindex title="404 | Seite nicht gefunden." />
+
+      <div className="sm:flex">
+        <p className="text-4xl sm:text-5xl font-extrabold text-orange-400">
+          404
+        </p>
+        <div className="sm:ml-6">
+          <div className="sm:border-l sm:border-gray-200 sm:pl-6">
+            <h1 className="text-4xl sm:text-5xl font-extrabold tracking-tight text-slate-900">
+              Diese Seite gibt es nicht
+            </h1>
+            <p className="mt-1 text-base text-slate-500">
+              Vielleicht ist ein Fehler in der URL?
+            </p>
+          </div>
+          <div className="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
+            <Link className="btn btn-brand" to="/">
+              Zur Startseite
+            </Link>
+          </div>
+        </div>
+      </div>
+    </LayoutArticle>
+  )
 }
 
-export default IndexPage
+export default NotFound
