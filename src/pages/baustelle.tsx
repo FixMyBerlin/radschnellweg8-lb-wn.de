4b3b588bd426c1bd863b6dc3c22fe9f48084a191
Tobias <tobias@fixmycity.de>
2022-09-19 10:43:19 +0200

    Cleanup unused Baustelle-Page
    Delete 404.tsx

--- src/pages/baustelle.tsx
+++
@@ -1,9 +0,0 @@
-import { PageProps } from 'gatsby'
-import React from 'react'
-import { Baustelle } from '~/components/Baustelle'
-
-const IndexPage: React.FC<PageProps> = () => {
-  return <Baustelle />
-}
-
-export default IndexPage
