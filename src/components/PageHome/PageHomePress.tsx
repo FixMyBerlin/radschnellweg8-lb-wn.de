e72f17fb271dc6564810e25050f5a32fe6d3247b
Tobias <tobias@fixmycity.de>
2022-11-21 09:31:25 +0100

    Update Press

--- src/components/PageHome/PageHomePress.tsx
+++ src/components/PageHome/PageHomePress.tsx
@@ -7,19 +7,32 @@ export const PageHomePress: React.FC = () => {
     <Section>
       <H2>Pressemitteilungen</H2>
 
-      <ul>
-        <li>
-          <strong>7.5.2019 | </strong>
-          <LinkExternal href="https://www.rems-murr-kreis.de/bauen-umwelt-und-verkehr/aktuelles/?tx_hwnews_hwnews%5BnewsartikelId%5D=1043&tx_hwnews_hwnews%5BcurrentPage%5D=2&tx_hwnews_hwnews%5Baction%5D=show&tx_hwnews_hwnews%5Bcontroller%5D=Newsartikel&cHash=2eb7bb05a868847505a7b59090e3f7dc">
-            Grünes Licht für Radschnellverbindung im Rems-Murr-Kreis:
-            Rems-Murr-Kreis
-          </LinkExternal>
-        </li>
-        <li>
-          <strong>25.05.2020 | </strong>
+      <ul className="space-y-1">
+        <li className="flex">
+          <strong className="mr-2 block min-w-[7rem] border-r">
+            25.05.2020
+          </strong>
           <LinkExternal href="https://www.landkreis-ludwigsburg.de/de/landratsamt-landkreis/aktuelles/pressemitteilungen/detail/nachhaltige-mobilitaet-zwischen-den-landkreisen/">
             Nachhaltige Mobilität zwischen den Landkreisen - Landratsamt
             Ludwigsburg
+          </LinkExternal>
+        </li>
+        <li className="flex">
+          <strong className="mr-2 block min-w-[7rem] border-r">
+            20.05.2020
+          </strong>
+          <LinkExternal href="https://www.rems-murr-kreis.de/landratsamt-und-politik/aktuelles?tx_hwnews_hwnews%5Baction%5D=show&tx_hwnews_hwnews%5Bcontroller%5D=Newsartikel&tx_hwnews_hwnews%5BnewsartikelId%5D=1321">
+            Rückenwind für die Planung des Radschnellwegs zwischen Ludwigsburg
+            und Waiblingen
+          </LinkExternal>
+        </li>
+        <li className="flex">
+          <strong className="mr-2 block min-w-[7rem] border-r">
+            07.05.2019
+          </strong>
+          <LinkExternal href="https://www.rems-murr-kreis.de/bauen-umwelt-und-verkehr/aktuelles/?tx_hwnews_hwnews%5BnewsartikelId%5D=1043&tx_hwnews_hwnews%5BcurrentPage%5D=2&tx_hwnews_hwnews%5Baction%5D=show&tx_hwnews_hwnews%5Bcontroller%5D=Newsartikel">
+            Grünes Licht für Radschnellverbindung im Rems-Murr-Kreis:
+            Rems-Murr-Kreis
           </LinkExternal>
         </li>
       </ul>
