d9fb1bdc8ef584bbd1df9a7d4ef26925b78250d0
Tobias <tobias@fixmycity.de>
2022-11-28 09:46:04 +0100

    Fix missing key warning

--- src/components/PageHome/PageHomeFunding.tsx
+++ src/components/PageHome/PageHomeFunding.tsx
@@ -6,22 +6,6 @@ import RadschnellwegeLogo from '../../images/bw-radschnellweg-logo.svg'
 import BmdvFoerderungLogo from '../../images/bmdv-foerderung-logo.svg'
 
 export const PageHomeFunding: React.FC = () => {
-  const images = [
-    <RadschnellwegeLogo
-      title="RadKULTUR Baden-Württemberg; RadSCHNELLWEG"
-      className="w-32 lg:w-[70%]"
-    />,
-    <BmdvFoerderungLogo
-      title="Bundesministerium für Digitales und Verkehr; aufgrund eines Beschlusses des Deutschen Bundestages"
-      className="w-40 lg:w-full"
-    />,
-    <StaticImage
-      src="../../images/vm-bw-foerderung-logo.png"
-      alt="Ministerium für Verkehr Baden-Württemberg"
-      className="w-40 lg:w-full"
-    />,
-  ]
-
   return (
     <Section className="pb-12">
       <H2 className="mt-[150px] mb-5 text-center !text-base !font-normal text-[#959595]">
@@ -29,7 +13,19 @@ export const PageHomeFunding: React.FC = () => {
       </H2>
 
       <div className="flex flex-col items-center gap-5 lg:flex-row lg:justify-center lg:gap-20">
-        {images.map((img) => img)}
+        <RadschnellwegeLogo
+          title="RadKULTUR Baden-Württemberg; RadSCHNELLWEG"
+          className="w-32 lg:w-[70%]"
+        />
+        <BmdvFoerderungLogo
+          title="Bundesministerium für Digitales und Verkehr; aufgrund eines Beschlusses des Deutschen Bundestages"
+          className="w-40 lg:w-full"
+        />
+        <StaticImage
+          src="../../images/vm-bw-foerderung-logo.png"
+          alt="Ministerium für Verkehr Baden-Württemberg"
+          className="w-40 lg:w-full"
+        />
       </div>
     </Section>
   )
