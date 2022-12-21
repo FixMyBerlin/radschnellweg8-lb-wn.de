ea523317eaaf3451c1ec363928fb8bb4b210e5b5
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2022-12-21 12:07:43 +0100

    Change width of SectionHeader

--- src/components/PageRoute/Section/SectionHeader.tsx
+++ src/components/PageRoute/Section/SectionHeader.tsx
@@ -7,11 +7,14 @@ type Props = {
 
 export const SectionHeader: React.FC<Props> = ({ header }) => {
   return (
-    <header id={header.id} className="mb-20 scroll-m-8">
+    <header
+      id={header.id}
+      className="mb-11 scroll-m-8 md:-ml-10 md:-mr-10 lg:-ml-48 lg:-mr-48"
+    >
       <h2>{header.title}</h2>
       <p>{header.owner}</p>
-      <p>{header.intro}</p>
-      {header.image}
+      <p className="mb-11">{header.intro}</p>
+      <div>{header.image}</div>
     </header>
   )
 }
