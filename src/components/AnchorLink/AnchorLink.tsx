7b316149e804602e21634883c43891668a55ac2a
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2022-12-22 10:24:59 +0100

    Update Anchor Link

--- src/components/AnchorLink/AnchorLink.tsx
+++ src/components/AnchorLink/AnchorLink.tsx
@@ -1,4 +1,5 @@
-import { ArrowUpIcon } from '@heroicons/react/24/outline'
+import { Transition } from '@headlessui/react'
+import { ArrowUpCircleIcon } from '@heroicons/react/24/outline'
 import React, { useEffect, useState } from 'react'
 import { Link } from '../Text'
 
@@ -7,23 +8,29 @@ type Props = {
 }
 
 export const AnchorLink: React.FC<Props> = ({ path = '' }) => {
-  const [offset, setOffset] = useState(0)
+  const [visible, setVisible] = useState(false)
 
   useEffect(() => {
-    if (typeof window !== `undefined`) {
-      window.onscroll = () => {
-        setOffset(window.pageYOffset)
-      }
+    const onScroll = (e) => {
+      setVisible(e.target.documentElement.scrollTop >= 150)
     }
-  }, [])
+    window.addEventListener('scroll', onScroll)
+    return () => window.removeEventListener('scroll', onScroll)
+  }, [visible])
 
   return (
-    <div>
-      {Boolean(offset) && offset >= 100 && (
-        <Link to={`${path}/#top`} className="mt-10 flex no-underline">
-          <ArrowUpIcon className="fixed top-[50%] right-8 z-20 w-10 rounded-full border-[3px] border-pink-500 bg-white p-1" />
-        </Link>
-      )}
-    </div>
+    <Transition
+      show={visible}
+      enter="transition-opacity duration-150"
+      enterFrom="opacity-0"
+      enterTo="opacity-100"
+      leave="transition-opacity duration-150"
+      leaveFrom="opacity-100"
+      leaveTo="opacity-0"
+    >
+      <Link to={`${path}/#top`} className="mt-10 flex no-underline">
+        <ArrowUpCircleIcon className="fixed bottom-16 right-8 z-50 w-14 rounded-full bg-white hover:text-rs8-pink" />
+      </Link>
+    </Transition>
   )
 }
