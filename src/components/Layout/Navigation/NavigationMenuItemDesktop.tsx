fdc7bb89f8daff6fdeff818931cc347437c41bd6
Tobias <tobias@fixmycity.de>
2022-12-22 12:05:31 +0100

    Navigation: Fix pink color of hover on primary menu

--- src/components/Layout/Navigation/NavigationMenuItemDesktop.tsx
+++ src/components/Layout/Navigation/NavigationMenuItemDesktop.tsx
@@ -16,8 +16,8 @@ export const NavigationMenuItemDesktop: React.FC<Props> = ({
       to={to}
       className={classNames(
         { 'text-rsv-blau font-bold': active },
-        { ' text-slate-500  hover:text-slate-700': !active },
-        'text-md before:bg-rsv-pink relative inline-flex items-center px-1 pt-1 font-medium leading-5 before:absolute before:bottom-0 before:left-0 before:block before:h-1 before:w-full before:origin-top-left before:scale-x-0 before:transition-transform before:duration-300 before:ease-out before:content-[""] before:hover:scale-x-100'
+        { 'text-slate-500 hover:text-slate-700': !active },
+        'text-md relative inline-flex items-center px-1 pt-1 font-medium leading-5 before:absolute before:bottom-0 before:left-0 before:block before:h-1 before:w-full before:origin-top-left before:scale-x-0 before:bg-rs8-pink before:transition-transform before:duration-300 before:ease-out before:content-[""] before:hover:scale-x-100'
       )}
     >
       {name}
