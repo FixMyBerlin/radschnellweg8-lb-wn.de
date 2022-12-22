7f10d510c39a76b556ce5cdfa6d77d37fc927bcb
Tobias <tobias@fixmycity.de>
2022-12-22 11:54:15 +0100

    Cleanup section-details
    Revert 29aec6e, not needed anymore after 4b82a4f

--- src/components/PageRoute/Section/SectionDetails.tsx
+++ src/components/PageRoute/Section/SectionDetails.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export const SectionDetails: React.FC<Props> = ({ details }) => {
   return (
-    <div className="section-details m-auto mb-20 space-y-6 divide-y divide-white border-y bg-gray-200 px-6 pb-6 md:mx-10 md:px-10">
+    <div className="m-auto mb-20 space-y-6 divide-y divide-white border-y bg-gray-200 px-6 pb-6 md:mx-10 md:px-10">
       <h3 className="mt-14 mb-8 text-center text-[32px] font-bold text-sky-700">
         Detailinfos
       </h3>
