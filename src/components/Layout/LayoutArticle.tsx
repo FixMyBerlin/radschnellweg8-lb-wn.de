ed347180bcb8a5298c6e810f4948d0289a2613e9
Tobias <tobias@fixmycity.de>
2022-12-26 14:55:23 +0100

    Remove duplicate ScrollTopLink

--- src/components/Layout/LayoutArticle.tsx
+++ src/components/Layout/LayoutArticle.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Layout } from '~/components/Layout'
-import { ScrollTopLink } from './ScrollTopLink'
 
 type Props = {
   location?: any // TODO: define type
@@ -10,7 +9,6 @@ type Props = {
 export const LayoutArticle: React.FC<Props> = ({ location, children }) => {
   return (
     <Layout location={location}>
-      <ScrollTopLink />
       <article className="prose mx-auto max-w-[950px] px-3 pt-10">
         {children}
       </article>
