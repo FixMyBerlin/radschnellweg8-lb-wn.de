003ce17197a96b85f78a16c9500f3aa05624e961
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2022-12-28 16:59:05 +0100

    Update PageRouteIntro.tsx

--- src/components/PageRoute/PageRouteIntro.tsx
+++ src/components/PageRoute/PageRouteIntro.tsx
@@ -1,8 +1,6 @@
-import { StaticImage } from 'gatsby-plugin-image'
 import React from 'react'
 import { Section } from '../Layout'
 import { P } from '../Text'
-import { Lightbox } from './Lightbox'
 
 export const PageRouteIntro: React.FC = () => (
   <Section>
@@ -13,15 +11,6 @@ export const PageRouteIntro: React.FC = () => (
       des Rems-Murr-Kreises mit dem Landkreis Ludwigsburg, der Stadt
       Ludwigsburg, der Stadt Waiblingen und der Stadt Remseck am Neckar.
     </P>
-
-    <Lightbox
-      image={
-        <StaticImage
-          src="./assets/TS2Detailpkt4.1-Unterfuehrung_K1666_LRA-LB.PNG"
-          alt="Bestehendes Radroutennetz der Stadt Ludwigsburg. © Stadt Ludwigsburg"
-        />
-      }
-    />
     <P>
       Unten finden Sie jeweils eine Beschreibung der aktuell geplanten
       Routenführung sowie Detailhinweise zu jeder Teilstrecke.
