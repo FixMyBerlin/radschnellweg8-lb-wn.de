82caaef98498e8cdd30dd96d57e017fc6431e432
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-05-03 15:11:33 +0200

    update texts and quote slider details on home page

--- src/components/PageHome/PageHomeIntro.tsx
+++ src/components/PageHome/PageHomeIntro.tsx
@@ -11,16 +11,17 @@ export const PageHomeIntro: React.FC = () => {
         oder gar Absteigen zwingen, mit gutem Belag und breit genug, dass man
         sicher überholen kann – so sollen sie aussehen, diese attraktiven
         Radrouten, die insbesondere Pendlerinnen und Pendler zum
-        klimafreundlichen und gesunden Umstieg aufs Rad animieren sollen.
+        klimafreundlichen und gesunden Umstieg aufs Rad animieren sollen. Wie
+        zum Beispiel beim geplanten Radschnellweg RS 8.
       </P>
 
       <P>
-        Der Radschnellweg RS 8 verbindet die Städte Ludwigsburg, Remseck und
+        Der RS 8 verbindet die Städte Ludwigsburg, Remseck am Neckar und
         Waiblingen. Auf diesen Seiten erhalten Sie Informationen zu
         Anforderungen, Planungsstand und Beteiligungsmöglichkeiten dieses
         Gemeinschaftsprojekts des Rems-Murr-Kreises mit dem Landkreis
-        Ludwigsburg, der Stadt Ludwigsburg, der Stadt Waiblingen und der Stadt
-        Remseck am Neckar.
+        Ludwigsburg, der Stadt Ludwigsburg, der Stadt Remseck am Neckar und der
+        Stadt Waiblingen.
       </P>
     </Section>
   )
