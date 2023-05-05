af67e7ff8007e01c3034dbdabe2bd684305ef42a
Henri <henri@fixmycity.de>
2023-05-05 11:35:17 +0200

    Link Logos to cities and county

--- src/components/Layout/Navigation/LogoBar/LogoBar.tsx
+++ src/components/Layout/Navigation/LogoBar/LogoBar.tsx
@@ -1,35 +1,46 @@
 import React from 'react'
+import { LinkExternal } from '~/components/Link'
 
 export const LogoBar: React.FC = () => {
   return (
     <div className="z-[1] w-full py-4 px-2 sm:p-8">
       <span className="sr-only">Projektpartner:</span>
       <div className="overflow-scoll flex flex-wrap gap-x-4 sm:justify-center sm:gap-x-8">
-        <img
-          className="h-[35px] w-auto lg:h-[40px]"
-          src="/layout-logo-bar/rems-murr-kreis-logo.jpg"
-          alt="Rems-Ruhr-Kreis"
-        />
-        <img
-          className="h-[35px] w-auto lg:h-[40px]"
-          src="/layout-logo-bar/landkreis-ludwigsburg-logo.svg"
-          alt="Landkreis Ludwigsburg"
-        />
-        <img
-          className="h-[35px] w-auto lg:h-[40px]"
-          src="/layout-logo-bar/ludwigsburg-logo.svg"
-          alt="Ludwigsburg"
-        />
-        <img
-          className="h-[35px] w-auto lg:h-[40px]"
-          src="/layout-logo-bar/stadt-waiblingen-logo.svg"
-          alt="Stadt Waiblingen"
-        />
-        <img
-          className="h-[35px] w-auto lg:h-[40px]"
-          src="/layout-logo-bar/remseck-logo.svg"
-          alt="Remseck"
-        />
+        <LinkExternal href="https://www.rems-murr-kreis.de/">
+          <img
+            className="h-[35px] w-auto lg:h-[40px]"
+            src="/layout-logo-bar/rems-murr-kreis-logo.jpg"
+            alt="Rems-Ruhr-Kreis"
+          />
+        </LinkExternal>
+        <LinkExternal href="https://www.landkreis-ludwigsburg.de/">
+          <img
+            className="h-[35px] w-auto lg:h-[40px]"
+            src="/layout-logo-bar/landkreis-ludwigsburg-logo.svg"
+            alt="Landkreis Ludwigsburg"
+          />
+        </LinkExternal>
+        <LinkExternal href="https://www.ludwigsburg.de/">
+          <img
+            className="h-[35px] w-auto lg:h-[40px]"
+            src="/layout-logo-bar/ludwigsburg-logo.svg"
+            alt="Ludwigsburg"
+          />
+        </LinkExternal>
+        <LinkExternal href="https://www.stadt-remseck.de/">
+          <img
+            className="h-[35px] w-auto lg:h-[40px]"
+            src="/layout-logo-bar/remseck-logo.svg"
+            alt="Remseck"
+          />
+        </LinkExternal>
+        <LinkExternal href="https://www.waiblingen.de/">
+          <img
+            className="h-[35px] w-auto lg:h-[40px]"
+            src="/layout-logo-bar/stadt-waiblingen-logo.svg"
+            alt="Stadt Waiblingen"
+          />
+        </LinkExternal>
       </div>
     </div>
   )
