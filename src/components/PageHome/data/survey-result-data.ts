fe647c2586c9e99a7654a6680451cab03534336a
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-08-18 12:07:44 +0200

    move survey result data to file, add documentation for data update

---
+++ src/components/PageHome/data/survey-result-data.ts
@@ -0,0 +1,40 @@
+export const surveyResultData = [
+  {
+    questionLabel: 'Würden Sie den RS 8 nutzen?',
+    data: [
+      { name: 'Ja', value: 9 },
+      { name: 'Nein', value: 88 },
+      {
+        name: 'Ich bin ohnehin nicht zwischen Ludwigsburg und Waiblingen unterwegs.',
+        value: 31,
+      },
+    ],
+  },
+  {
+    questionLabel: 'Wie häufig würden Sie den RS 8 nutzen?',
+    data: [
+      { name: 'Täglich', value: 39 },
+      { name: 'Mehrmals pro Woche', value: 10 },
+      { name: 'Mehrmals im Monat', value: 14 },
+      { name: 'Seltener oder Nie', value: 15 },
+    ],
+  },
+  {
+    questionLabel: 'Für welche Zwecke würden Sie den RS 8 nutzen?',
+    data: [
+      { name: 'Einkaufen', value: 57 },
+      { name: 'Zur Arbeit/Schule pendeln', value: 110 },
+      { name: 'Sport/Freizeit', value: 30 },
+      { name: 'Anderes', value: 15 },
+    ],
+  },
+  {
+    questionLabel: 'Würden Sie durch den RS 8 häufiger aufs Auto verzichten?',
+    data: [
+      { name: 'Ja', value: 185 },
+      { name: 'Nein', value: 16 },
+      { name: 'Ich verzichte bereits aufs Auto.', value: 28 },
+      { name: 'Weiß nicht / Keine Angabe', value: 32 },
+    ],
+  },
+]
