f24d3c79b2254391d82f1958417d4a63585a7c35
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-08-31 15:37:04 +0200

    fix tailwind config: do not overwrite tw font sizes

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,18 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx,svg}'],
   theme: {
     extend: {
+      fontSize: {
+        sm: ['14px', '20px'],
+        base: ['18px', '32px'],
+        lg: ['20px', '28px'],
+        xl: ['24px', '32px'],
+      },
       colors: {
         'rs8-blue': '#2C62A9',
         'rs8-pink': '#E5007D',
       },
     },
     fontFamily: { sans: ['Overpass Variable', 'sans-serif'] },
-    fontSize: {
-      sm: ['14px', '20px'],
-      base: ['18px', '32px'],
-      lg: ['20px', '28px'],
-      xl: ['24px', '32px'],
-    },
   },
   plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
 }
