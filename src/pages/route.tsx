29ca35a19f9598cd4370abc5d63f5b8b6f719bdb
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2023-08-31 15:38:41 +0200

    rename PageRouteSection and clean up layout

--- src/pages/route.tsx
+++ src/pages/route.tsx
@@ -1,10 +1,11 @@
 import { PageProps } from 'gatsby'
 import React from 'react'
-import { Layout } from '~/components/Layout'
+import { Layout, Section } from '~/components/Layout'
 import { Link } from '~/components/Link'
 import { RouteMap } from '~/components/Maps/RouteMap/RouteMap'
 import { MetaTags } from '~/components/MetaTags/MetaTags'
 import { PageRouteIntro } from '~/components/PageRoute'
+import { PageRouteSection } from '~/components/PageRoute/Section/PageRouteSection'
 import {
   section1Details,
   section1Header,
@@ -15,7 +16,6 @@ import {
   section4Details,
   section4Header,
 } from '~/components/PageRoute/data.const'
-import { Section } from '~/components/PageRoute/Section'
 import { HeadingContentPage } from '~/components/Text'
 
 export const Head = () => <MetaTags />
@@ -24,28 +24,29 @@ const Route: React.FC<PageProps> = ({ location }) => {
   const menu = [section1Header, section2Header, section3Header, section4Header]
 
   return (
-    <Layout className="mx-auto max-w-[950px] px-3 pt-10" location={location}>
+    <Layout location={location}>
       <HeadingContentPage>Route</HeadingContentPage>
 
       <PageRouteIntro />
+      <Section>
+        <div className="my-10">
+          <RouteMap sectionNumbers />
+        </div>
 
-      <div className="my-10">
-        <RouteMap sectionNumbers />
-      </div>
-
-      <ul className="marker:text-rs8-pink">
-        {menu.map(({ id, title }) => {
-          return (
-            <li>
-              <Link to={`#${id}`}>{title}</Link>
-            </li>
-          )
-        })}
-      </ul>
-      <Section header={section1Header} details={section1Details} />
-      <Section header={section2Header} details={section2Details} />
-      <Section header={section3Header} details={section3Details} />
-      <Section header={section4Header} details={section4Details} />
+        <ul className="marker:text-rs8-pink">
+          {menu.map(({ id, title }) => {
+            return (
+              <li>
+                <Link to={`#${id}`}>{title}</Link>
+              </li>
+            )
+          })}
+        </ul>
+      </Section>
+      <PageRouteSection header={section1Header} details={section1Details} />
+      <PageRouteSection header={section2Header} details={section2Details} />
+      <PageRouteSection header={section3Header} details={section3Details} />
+      <PageRouteSection header={section4Header} details={section4Details} />
     </Layout>
   )
 }
