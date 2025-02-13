05f44fa3ac4fb922c9cb28057fe2869dfc97ebb5
JohannaPeanut <76495099+JohannaPeanut@users.noreply.github.com>
2025-02-13 12:31:15 +0100

    Route: add socials and documents

--- shared/cms/routesegments.astro.tsx
+++ shared/cms/routesegments.astro.tsx
@@ -7,5 +7,22 @@ export const astroRoutesegmentsDefinition = defineCollection({
       title: z.string(),
       operator: z.string(),
       position: z.number(),
+      documents: z
+        .array(
+          z.object({
+            document: z.string(),
+            description: z.string().optional(),
+            title: z.string(),
+          }),
+        )
+        .optional(),
+      socials: z
+        .array(
+          z.object({
+            label: z.string(),
+            link: z.string(),
+          }),
+        )
+        .optional(),
     }),
 })
--- shared/cms/routesegments.keystatic.tsx
+++ shared/cms/routesegments.keystatic.tsx
@@ -11,6 +11,33 @@ export const keystaticRoutesegmentsConfig = collection({
     title: fields.slug({ name: { label: 'Titel' } }),
     operator: fields.text({ label: 'Baulastträger' }),
     position: fields.number({ label: 'Position' }),
+    documents: fields.array(
+      fields.object({
+        document: fields.file({
+          label: 'Dokument',
+          validation: { isRequired: true },
+          directory: 'public/files/',
+          publicPath: '/files/',
+        }),
+        description: fields.text({ label: 'Beschreibung' }),
+        title: fields.text({ label: 'Titel', validation: { isRequired: true } }),
+      }),
+
+      {
+        itemLabel: (props) => props.fields.title.value,
+        label: 'Dokumente',
+      },
+    ),
+    socials: fields.array(
+      fields.object({
+        label: fields.text({ label: 'Label', validation: { isRequired: true } }),
+        link: fields.url({ label: 'Link', validation: { isRequired: true } }),
+      }),
+      {
+        itemLabel: (props) => props.fields.label.value,
+        label: 'Social Media Links',
+      },
+    ),
     body: fields.mdx({
       label: 'Text',
       components: mdxComponentsKeystatic('routesegments'),
